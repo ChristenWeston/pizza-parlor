cb04e4e7c4abea39335d66550c603e946c73ae61
Christen Weston <paleeserecycle@gmail.com>
2021-12-11 12:40:45 -0800

    Added in prototype function for pizza cost

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Describe: Pizza()
 Test: "It should return a Pizza object with two properties for toppings and size"
 Code: const myPizza = new Pizza(["broccoli", "garlic"], "vegan cheese", "tomato", "medium");
 Expected Output: Pizza { toppings: ["broccoli", "garlic"], cheese: "vegan cheese", sauce: "tomato", size: "medium" }
+
+Describe: Pizza.prototype.pizzaOrder(pizza)
+
+Test: "It should return the cost of the pizza based on the number of toppings and the size."
+Code: Pizza.prototype.pizzaOrder(myPizza);
+Expected Output: 12.5;
--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,25 @@ function Pizza(toppings, cheese, sauce, size) {
   this.sauce = sauce;
   this.size = size;
 }
+
+Pizza.prototype.pizzaOrder = function(pizza) {
+  let cost = 0.00;
+  let numberOfToppings = parseInt(pizza.toppings.length) + 1;
+  let toppingsCost = numberOfToppings * 1.25;
+  
+  if (pizza.size === "small") {
+    cost += 7.00;
+  }
+  else if (pizza.size === "medium") {
+    cost += 10.00;
+  }
+  else if (pizza.size === "large") {
+    cost += 13.00;
+  }
+  else {
+    console.log("No pizza size!");
+  }
+
+  cost += toppingsCost;
+  return cost;
+}
